3e9275f3267b033c9db1509c1dd6d064a54995da
valentin <v.prokhorov@telecontact.ru>
2024-03-22 19:59:05 +0300

    (feat) добавлен новый вариант метода click_link_draft

--- pages/site_map_page.py
+++ pages/site_map_page.py
@@ -11,11 +11,34 @@ class SiteMapPage(BasePage):
     PAGE_URL = Links.SITE_MAP_LINK
 
     LINK_SITEMAP_STAT_COMMON = ('xpath', '//a[@id="link_sitemap_stat_common"]')
+    LINK_SITEMAP_STAT_COMMONS = ('xpath', '//a[@id="link_sitemap_stat_contacts"]')
+    LINK_HOME = ('xpath', '//a[@href="/development/testing/sc240314_0000_testing_prohorov_2920/SiteMap.php"]')
 
     def click_link(self):
         self.wait.until(EC.element_to_be_clickable(self.LINK_SITEMAP_STAT_COMMON)).click()
-        self.wait.until(EC.element_to_be_clickable(self.LINK_SITEMAP_STAT_COMMON)).click()
+        var = self.driver.current_url
+        print(var)
+        self.wait.until(EC.element_to_be_clickable(self.LINK_HOME)).click()
+        self.wait.until(EC.element_to_be_clickable(self.LINK_SITEMAP_STAT_COMMONS)).click()
+        self.wait.until(EC.element_to_be_clickable(self.LINK_HOME)).click()
+
         result = Http_methods.get(self.PAGE_URL)
+        # print(self.PAGE_URL)
         expect_equal(check_name="Код ответа сервера", actual_value=result.status_code,
                      expected_value=200)
 
+    def click_link_draft(self):
+        self.wait.until(EC.element_to_be_clickable(self.LINK_SITEMAP_STAT_COMMON)).click()
+        current_url = self.driver.current_url
+        response = Http_methods.get(current_url)
+        assert response.status_code == 200, f"Expected status code 200, but got {response.status_code} for URL {current_url}"
+
+        self.wait.until(EC.element_to_be_clickable(self.LINK_HOME)).click()
+        current_url = self.driver.current_url
+        response = Http_methods.get(current_url)
+        assert response.status_code == 200, f"Expected status code 200, but got {response.status_code} for URL {current_url}"
+
+        self.wait.until(EC.element_to_be_clickable(self.LINK_SITEMAP_STAT_COMMONS)).click()
+        current_url = self.driver.current_url
+        response = Http_methods.get(current_url)
+        assert response.status_code == 200, f"Expected status code 200, but got {response.status_code} for URL {current_url}"
