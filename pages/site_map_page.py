2daff03894225bb4ab2b94e39d15de244a04d3e1
valentin <v.prokhorov@telecontact.ru>
2024-03-23 14:43:29 +0300

    (feat) мелкие правки

--- pages/site_map_page.py
+++ pages/site_map_page.py
@@ -13,32 +13,39 @@ class SiteMapPage(BasePage):
     LINK_SITEMAP_STAT_COMMON = ('xpath', '//a[@id="link_sitemap_stat_common"]')
     LINK_SITEMAP_STAT_COMMONS = ('xpath', '//a[@id="link_sitemap_stat_contacts"]')
     LINK_HOME = ('xpath', '//a[@href="/development/testing/sc240314_0000_testing_prohorov_2920/SiteMap.php"]')
+    # BREADCRUMB_TITLE =  ('xpath', '//td[@class="notifications-text-td"]')
+    BREADCRUMB_TITLE =  ('xpath', '//span[@id="breadcrumb_title"]')
+
+    # def click_link(self):
+    #     self.wait.until(EC.element_to_be_clickable(self.LINK_SITEMAP_STAT_COMMON)).click()
+    #     var = self.driver.current_url
+    #     print(var)
+    #     self.wait.until(EC.element_to_be_clickable(self.LINK_HOME)).click()
+    #     self.wait.until(EC.element_to_be_clickable(self.LINK_SITEMAP_STAT_COMMONS)).click()
+    #     self.wait.until(EC.element_to_be_clickable(self.LINK_HOME)).click()
+    #
+    #     result = Http_methods.get(self.PAGE_URL)
+    #     # print(self.PAGE_URL)
+    #     expect_equal(check_name="Код ответа сервера", actual_value=result.status_code,
+    #                  expected_value=200)
 
     def click_link(self):
         self.wait.until(EC.element_to_be_clickable(self.LINK_SITEMAP_STAT_COMMON)).click()
-        var = self.driver.current_url
+        # self.wait.until(EC.text_to_be_present_in_element(self.BREADCRUMB_TITLE,'Для отображения данных задайте необходимые параметры и нажмите кнопку "Обновить".'))
+        var = self.wait.until(EC.title_is('sc240314_0000_testing_prohorov_2920 \ Статистики \ Общая статистика'))
         print(var)
-        self.wait.until(EC.element_to_be_clickable(self.LINK_HOME)).click()
-        self.wait.until(EC.element_to_be_clickable(self.LINK_SITEMAP_STAT_COMMONS)).click()
-        self.wait.until(EC.element_to_be_clickable(self.LINK_HOME)).click()
-
-        result = Http_methods.get(self.PAGE_URL)
-        # print(self.PAGE_URL)
-        expect_equal(check_name="Код ответа сервера", actual_value=result.status_code,
-                     expected_value=200)
-
-    def click_link_draft(self):
-        self.wait.until(EC.element_to_be_clickable(self.LINK_SITEMAP_STAT_COMMON)).click()
         current_url = self.driver.current_url
         response = Http_methods.get(current_url)
         assert response.status_code == 200, f"Expected status code 200, but got {response.status_code} for URL {current_url}"
 
-        self.wait.until(EC.element_to_be_clickable(self.LINK_HOME)).click()
-        current_url = self.driver.current_url
-        response = Http_methods.get(current_url)
-        assert response.status_code == 200, f"Expected status code 200, but got {response.status_code} for URL {current_url}"
-
-        self.wait.until(EC.element_to_be_clickable(self.LINK_SITEMAP_STAT_COMMONS)).click()
-        current_url = self.driver.current_url
-        response = Http_methods.get(current_url)
-        assert response.status_code == 200, f"Expected status code 200, but got {response.status_code} for URL {current_url}"
+        # self.wait.until(EC.element_to_be_clickable(self.LINK_HOME)).click()
+        # current_url = self.driver.current_url
+        # response = Http_methods.get(current_url)
+        # assert response.status_code == 200, \
+        #     f"Expected status code 200, but got {response.status_code} for URL {current_url}"
+        #
+        # self.wait.until(EC.element_to_be_clickable(self.LINK_SITEMAP_STAT_COMMONS)).click()
+        # current_url = self.driver.current_url
+        # response = Http_methods.get(current_url)
+        # assert response.status_code == 200, \
+        #     f"Expected status code 200, but got {response.status_code} for URL {current_url}"
